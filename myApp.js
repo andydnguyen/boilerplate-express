98e270332d4c311b425719ef9d6c6da7d49fe0ce
Andy Nguyen <anguyen1119@gmail.com>
2022-01-23 06:19:16 +0000

    Log every request "method path - ip"

--- myApp.js
+++ myApp.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next){
+  console.log(req.method, req.path, '-', req.ip)
+  next()
+})
+
+app.use('/public', express.static(__dirname + '/public'))
+
 app.get('/', function(req,res){
   // res.send('Hello Express')
   res.sendFile(__dirname + "/views/index.html")
 })
 
-app.use('/public', express.static(__dirname + '/public'))
+
 
 app.get('/json', function(req,res){
   if(process.env['MESSAGE_STYLE'] === 'uppercase'){
